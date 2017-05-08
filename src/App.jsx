ec42c263d9e566c2635a54e10477080a64241ffc
David Higgins <thehig@gmail.com>
2017-05-08 13:56:00 +0100

    Add react-router-dom@next

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-const App = () => <h1>Hello React</h1>;
+const Home = () => <h1>Home</h1>;
+const NotFound = () => <h1>NotFound</h1>;
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/*" component={NotFound} />
+    </Switch>
+  </Router>
+);
 
 export default App;
