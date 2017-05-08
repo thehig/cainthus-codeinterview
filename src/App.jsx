2474f4ca0bbf8b99b1728f555683067e217f61cd
David Higgins <thehig@gmail.com>
2017-05-08 13:59:42 +0100

    Add nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import './App.css';
 
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+  </nav>
+);
+
 const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
 const NotFound = () => <h1>NotFound</h1>;
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/*" component={NotFound} />
-    </Switch>
+    <div>
+      <Links />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/*" component={NotFound} />
+      </Switch>
+    </div>
   </Router>
 );
 
